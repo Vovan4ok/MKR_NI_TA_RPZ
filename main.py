8aadc36ac3bea0193aac67144ce1df8048b662dc
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-28 11:52:16 +0300

    add check_sample_info_in_lists method

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ def read_data_from_the_file(file_name):
 
     return file_info
 
+
 def write_data_into_file(file_name, info_to_write):
     file = open(file_name, "w")
     file.truncate()
     for line in info_to_write:
         file.write(line)
     file.close()
+
+
+def check_sample_info_in_lists(list1, list2):
+    sample_info = []
+    for elem in list1:
+        if elem in list2:
+            sample_info.append(elem)
+    return sample_info
+
 
 def main(file1_name, file2_name):
     print(read_data_from_the_file(file1_name))
