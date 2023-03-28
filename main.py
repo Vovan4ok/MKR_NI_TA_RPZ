7b160d1d4340ee740dd0f475228b338a6c8e5d64
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-28 12:08:19 +0300

    develop main method

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def write_data_into_file(file_name, info_to_write):
     file = open(file_name, "w")
     file.truncate()
     for line in info_to_write:
-        file.write(line)
+        file.write(line + "\n")
     file.close()
 
 
@@ -33,8 +33,17 @@ def check_diff_info_in_lists(list1, list2):
     return diff_info
 
 def main(file1_name, file2_name):
-    print("Hello world")
+    info_from_file1 = read_data_from_the_file(file1_name)
+    info_from_file2 = read_data_from_the_file(file2_name)
 
+    same_info = check_same_info_in_lists(info_from_file1, info_from_file2)
+
+    diff_info = check_diff_info_in_lists(info_from_file1, info_from_file2)
+    for elem in check_diff_info_in_lists(info_from_file2, info_from_file1):
+        diff_info.append(elem)
+
+    write_data_into_file("same.txt", same_info)
+    write_data_into_file("diff.txt", diff_info)
 
 if __name__ == '__main__':
     main("file1.txt", "file2.txt")
