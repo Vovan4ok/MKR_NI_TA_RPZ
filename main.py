e048ac34663755360d5d3f0513246a032075d8d4
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-28 11:39:04 +0300

    add read_data_from_file method

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
+def read_data_from_the_file(file_path):
+    file_info = []
+    with open(file_path) as file:
+        for line in file:
+            file_info.append(line)
+
+    return file_info
+
 
 def main(file1_path, file2_path):
     print("Hello world")
