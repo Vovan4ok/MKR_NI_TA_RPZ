0b2e96747db91df7227a0fea8ae4646cf2fcc4fd
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-28 11:51:34 +0300

    add write_data_into_file method

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 
-def read_data_from_the_file(file_path):
+def read_data_from_the_file(file_name):
     file_info = []
-    with open(file_path) as file:
+    with open(file_name) as file:
         for line in file:
             file_info.append(line)
 
     return file_info
 
+def write_data_into_file(file_name, info_to_write):
+    file = open(file_name, "w")
+    file.truncate()
+    for line in info_to_write:
+        file.write(line)
+    file.close()
 
-def main(file1_path, file2_path):
-    print("Hello world")
+def main(file1_name, file2_name):
+    print(read_data_from_the_file(file1_name))
+    print(read_data_from_the_file(file2_name))
 
 
 if __name__ == '__main__':
-    main("sjad", "sdjasdk")
+    main("file1.txt", "file2.txt")
