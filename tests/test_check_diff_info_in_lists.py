514f994dd36437f27bed7abc788f5a15a77a4a15
Volodymyr Zganyayko <bosik_ck@ukr.net>
2023-03-28 12:35:30 +0300

    fix tests problem

--- tests/test_check_diff_info_in_lists.py
+++ tests/test_check_diff_info_in_lists.py
@@ -7,6 +7,6 @@ from main import check_diff_info_in_lists
     ([5, 8, 1, 3], [1, 8, 3, 6], [5]),
     ([1, 2, 3, 4, 8], [3, 2, 1, 5, 7], [4, 8])
 ])
-def test_check_diff_info(list1, list2, result):
+def test_check_diff_info_in_lists(list1, list2, result):
     check = check_diff_info_in_lists(list1, list2)
     assert check == result
--- tests/test_check_same_info_in_lists.py
+++ tests/test_check_same_info_in_lists.py
@@ -7,6 +7,6 @@ from main import check_same_info_in_lists
     ([5, 8, 1, 3], [1, 8, 3, 6], [8, 1, 3]),
     ([1, 2, 3, 4], [3, 2, 1, 5, 7], [1, 2, 3])
 ])
-def test_check_same_info(list1, list2, result):
+def test_check_same_info_in_lists(list1, list2, result):
     check = check_same_info_in_lists(list1, list2)
     assert check == result
